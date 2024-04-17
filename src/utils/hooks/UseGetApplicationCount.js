949721004e10162e4a4ee449391dd09b21b257f7
jayanta <jayanta.garu@arithaconsulting.com>
2024-04-17 12:20:16 +0530

    code update

--- src/utils/hooks/UseGetApplicationCount.js
+++
@@ -1,6 +0,0 @@
-import { useSelector } from 'react-redux';
-
-const UseGetApplicationCount = () =>
-  useSelector((state) => state?.dashboard?.dashboardInfo?.applications?.length);
-
-export default UseGetApplicationCount;
