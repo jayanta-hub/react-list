f18700cfc208f09d86cff8d74e4064f9f3aefc53
jayanta-hub <jayanta.garu@gmail.com>
2024-09-03 13:34:15 +0530

    v

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, TaskList } from "../../components/index.jsx";
+import { fetchWithRetry } from "../../utils/helpers/index.jsx";
 
 const Dashboard = () => {
+  const [apiData,setApiData]=useState("")
+  const [isLoding,setIsloading]=useState(false)
+  const [isError,setIsError]=useState("")
+  const fetchData=async ()=>{
+    setIsloading(true)
+    try{
+      const result=await fetchWithRetry("https://dummyjson.com/produc",{},3,2000)
+      setApiData(result);
+    }catch(err){
+      setIsError(err?.message||"Something went wrong.")
+    } finally{
+      setIsloading(false)
+    }
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
+  if(isError){
+    return <div>Error:{isError}</div>
+  }
+  if(isLoding){
+    return <div>Loading...</div>
+  }
   return (
     <Box className="container items-center py-5">
-      <TaskList />
+      {JSON.stringify(apiData)}
+      {/* <TaskList /> */}
     </Box>
   );
 };
--- src/utils/helpers/index.jsx
+++ src/utils/helpers/index.jsx
@@ -1,1 +1,19 @@
 export const uuid = () => (Math.random() + 1).toString(36).substring(2);
+
+export const fetchWithRetry=async (url,option={},retry=3,retryDelay=1000)=>{
+    let attempt=0;
+    while(attempt<retry){
+        try{
+            const res=await fetch(url,option);
+            console.log("h",res)
+            if(!res.ok) throw new Error(`HTTP error! status: ${res.statusText}`);
+                return await res.json();
+        }catch(err){
+            console.log("k",err)
+            attempt++
+            if(attempt>=retry) throw err
+        }
+        await new Promise((resolve)=>setTimeout(resolve,retryDelay))
+    }
+
+}
