3b47fdb6b8ab380d5d374b53c6712877e3b3c1bc
Rupesh Mahanta <117063501+RupeshMahanta1994@users.noreply.github.com>
2024-09-05 15:09:04 +0530

    asd

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -3,33 +3,42 @@ import { Box, TaskList } from "../../components/index.jsx";
 import { fetchWithRetry } from "../../utils/helpers/index.jsx";
 
 const Dashboard = () => {
-  const [apiData,setApiData]=useState("")
-  const [isLoding,setIsloading]=useState(false)
-  const [isError,setIsError]=useState("")
-  const fetchData=async ()=>{
-    setIsloading(true)
-    try{
-      const result=await fetchWithRetry("https://dummyjson.com/produc",{},3,2000)
+  const [apiData, setApiData] = useState("");
+  const [isLoding, setIsloading] = useState(false);
+  const [isError, setIsError] = useState("");
+  const fetchData = async () => {
+    setIsloading(true);
+    try {
+      const result = await fetchWithRetry(
+        "https://dummyjson.com/produc",
+        {},
+        3,
+        2000
+      )
+        .then((res) => console.log(res, "res"))
+        .catch((err) => {
+          console.log("errsss", err);
+        });
       setApiData(result);
-    }catch(err){
-      setIsError(err?.message||"Something went wrong.")
-    } finally{
-      setIsloading(false)
+    } catch (err) {
+      setIsError(err?.message || "Something went wrong.");
+    } finally {
+      setIsloading(false);
     }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  if (isError) {
+    return <div>Error:{isError}</div>;
   }
-  useEffect(()=>{
-    fetchData()
-  },[])
-  if(isError){
-    return <div>Error:{isError}</div>
-  }
-  if(isLoding){
-    return <div>Loading...</div>
+  if (isLoding) {
+    return <div>Loading...</div>;
   }
   return (
     <Box className="container items-center py-5">
       {JSON.stringify(apiData)}
-      {/* <TaskList /> */}
+      <TaskList />
     </Box>
   );
 };
--- src/utils/helpers/index.jsx
+++ src/utils/helpers/index.jsx
@@ -1,19 +1,23 @@
 export const uuid = () => (Math.random() + 1).toString(36).substring(2);
 
-export const fetchWithRetry=async (url,option={},retry=3,retryDelay=1000)=>{
-    let attempt=0;
-    while(attempt<retry){
-        try{
-            const res=await fetch(url,option);
-            console.log("h",res)
-            if(!res.ok) throw new Error(`HTTP error! status: ${res.statusText}`);
-                return await res.json();
-        }catch(err){
-            console.log("k",err)
-            attempt++
-            if(attempt>=retry) throw err
-        }
-        await new Promise((resolve)=>setTimeout(resolve,retryDelay))
+export const fetchWithRetry = async (
+  url,
+  option = {},
+  retry = 3,
+  retryDelay = 1000
+) => {
+  let attempt = 0;
+  while (attempt < retry) {
+    try {
+      const res = await fetch(url, option);
+      if (!res.ok) throw new Error(`HTTP error! status: ${res.statusText}`);
+      return await res.json();
+    } catch (err) {
+      attempt++;
+      if (attempt >= retry) throw err;
     }
-
-}
+    await new Promise((resolve) =>
+      setTimeout(() => resolve("asn"), retryDelay)
+    );
+  }
+};
